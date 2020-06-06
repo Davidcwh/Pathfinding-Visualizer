5db2f64de8c88f9e23549e77277ab21fd5d372c5
davidcwh <dcwh12321@gmail.com>
2020-06-06 14:00:20 +0800

    resize nodes

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Grid from './Grid';
+import Menu from './Menu';
 import '../css/App.css';
 
 class App extends React.Component {
@@ -7,6 +8,7 @@ class App extends React.Component {
     render() {
         return (
             <div id="wrapper" className="App">
+                <Menu/>
                 <Grid />
             </div>
         );
