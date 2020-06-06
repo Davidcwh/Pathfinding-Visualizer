59029acd5c09536e3c0349f2455b50b2bc3f3a69
davidcwh <dcwh12321@gmail.com>
2020-06-06 14:01:02 +0800

    create menu component

---
+++ src/components/Menu.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+class Menu extends React.Component {
+    render() {
+        return <div><h1>Hello There</h1></div>
+    }
+}
+
+export default Menu;
