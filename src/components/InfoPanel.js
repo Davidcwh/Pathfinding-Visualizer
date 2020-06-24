c8b03525daaa7e75393fc7a086d9aaeaf278ed09
davidcwh <dcwh12321@gmail.com>
2020-06-25 01:11:27 +0800

    Add web icon and fix className warning

--- src/components/InfoPanel.js
+++ src/components/InfoPanel.js
@@ -18,58 +18,58 @@ class InfoPanel extends React.Component {
             <div style={{width: "85%", margin: "auto", marginTop: "2vh"}}>
                 <div className="ui small eight statistics">
     
-                    <div class="statistic">
-                        <div class="value">
+                    <div className="statistic">
+                        <div className="value">
                             <div className={`node node-start`} style={{border: "2px solid white", borderRadius: "5px"}} />
                         </div>
-                        <div class="label">start</div>
+                        <div className="label">start</div>
                     </div>
     
-                    <div class="statistic">
-                        <div class="value">
+                    <div className="statistic">
+                        <div className="value">
                             <div className={`node node-finish`} style={{border: "2px solid white", borderRadius: "5px"}} />
                         </div>
-                        <div class="label">end</div>
+                        <div className="label">end</div>
                     </div>
     
-                    <div class="statistic">
-                        <div class="value">{displayValue(unvisited)} <div className={`node node-unvisited`} style={{border: "2px solid white", borderRadius: "5px"}} /></div>
-                        <div class="label">
+                    <div className="statistic">
+                        <div className="value">{displayValue(unvisited)} <div className={`node node-unvisited`} style={{border: "2px solid white", borderRadius: "5px"}} /></div>
+                        <div className="label">
                             <div>unvisited</div>
                         </div>
                     </div>
     
-                    <div class="statistic">
-                        <div class="value">{displayValue(wall)} <div className={`node node-wall`} style={{border: "2px solid white", borderRadius: "5px"}} /></div>
-                        <div class="label">
+                    <div className="statistic">
+                        <div className="value">{displayValue(wall)} <div className={`node node-wall`} style={{border: "2px solid white", borderRadius: "5px"}} /></div>
+                        <div className="label">
                             <div>wall</div>
                         </div>
                     </div>
     
-                    <div class="statistic">
-                        <div class="value">{displayValue(visited)} <div className={`node node-visited`} style={{border: "2px solid white", borderRadius: "5px"}} /></div>
-                        <div class="label">
+                    <div className="statistic">
+                        <div className="value">{displayValue(visited)} <div className={`node node-visited`} style={{border: "2px solid white", borderRadius: "5px"}} /></div>
+                        <div className="label">
                             <div>visited</div>
                         </div>
                     </div>
     
-                    <div class="statistic">
-                        <div class="value">{displayValue(backtrack)} <div className={`node node-backtrack`} style={{border: "2px solid white", borderRadius: "5px"}} /></div>
-                        <div class="label">
+                    <div className="statistic">
+                        <div className="value">{displayValue(backtrack)} <div className={`node node-backtrack`} style={{border: "2px solid white", borderRadius: "5px"}} /></div>
+                        <div className="label">
                             <div>backtrack</div>
                         </div>
                     </div>
     
-                    <div class="statistic">
-                        <div class="value">{displayValue(frontier)} <div className={`node node-frontier`} style={{border: "2px solid white", borderRadius: "5px"}} /></div>
-                        <div class="label">
+                    <div className="statistic">
+                        <div className="value">{displayValue(frontier)} <div className={`node node-frontier`} style={{border: "2px solid white", borderRadius: "5px"}} /></div>
+                        <div className="label">
                             <div>frontier</div>
                         </div>
                     </div>
     
-                    <div class="statistic">
-                        <div class="value">{displayValue(path)} <div className={`node node-path`} style={{border: "2px solid white", borderRadius: "5px"}} /></div>
-                        <div class="label">
+                    <div className="statistic">
+                        <div className="value">{displayValue(path)} <div className={`node node-path`} style={{border: "2px solid white", borderRadius: "5px"}} /></div>
+                        <div className="label">
                             <div>path</div>
                         </div>
                     </div>
